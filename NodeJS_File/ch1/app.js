35455e93716fa42b8b97d7c20d4f50cb38215c66
shch989 <tankpop11@gmail.com>
2023-02-18 23:41:39 +0900

    ch1 진행

---
+++ NodeJS_File/ch1/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const port = 5000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/', (req, res, next) => {
+  console.log('This always runs!')
+  next()
+})
+
+app.use('/add-product', (req, res) => {
+  console.log('In another middleware!')
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+  )
+})
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use('/', (req, res) => {
+  console.log('In another middleware!')
+  res.send('<h1>Hello from Express!</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/`)
+})
