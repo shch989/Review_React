b358f7d7aba75dd30c8176d9737d28ac4aef5d00
shch989 <tankpop11@gmail.com>
2023-02-19 16:53:57 +0900

    ch2 요약 시작

--- NodeJS_File/ch1/app.js
+++ NodeJS_File/ch1/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -5,28 +6,17 @@ const app = express()
 
 const port = 5000
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs!')
-  next()
-})
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-app.use('/add-product', (req, res) => {
-  console.log('In another middleware!')
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
-  )
-})
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body)
-  res.redirect('/')
-})
-
-app.use('/', (req, res) => {
-  console.log('In another middleware!')
-  res.send('<h1>Hello from Express!</h1>')
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(port, () => {
---
+++ NodeJS_File/ch2/ex1/app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 5000
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}/`)
+  })
+  
