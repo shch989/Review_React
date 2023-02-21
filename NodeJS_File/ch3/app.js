a82596891df60129073d1888318b4e6e19c62d3f
shch989 <tankpop11@gmail.com>
2023-02-21 23:22:59 +0900

    mvc 패턴 정리

---
+++ NodeJS_File/ch3/app.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error')
+
+const app = express();
+
+const port = 5000
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/`)
+})
