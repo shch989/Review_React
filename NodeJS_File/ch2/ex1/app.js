b038bf1085d67ab6eb4d565250b193458def777d
shch989 <tankpop11@gmail.com>
2023-02-21 21:03:39 +0900

    pug handlerbar 사용

--- NodeJS_File/ch2/ex1/app.js
+++ NodeJS_File/ch2/ex1/app.js
@@ -5,15 +5,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 const port = 5000
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
